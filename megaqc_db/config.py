859f005bb57762697ff91db56bceddbcbed794e8
Kento-Bento <71308285+Kento-Bento@users.noreply.github.com>
2020-10-11 12:08:37 +1100

    Config
    For now, if you want to deploy the database locally, you will have to manually change the 'DATABASE_URI' global variable to be able to log into your database of choice

---
+++ megaqc_db/config.py
@@ -0,0 +1,11 @@
+### config.py ###
+
+# For now, to test depolyment of this database locally, you will have to download the megaqc_db repo, 
+# and set the URI below to log into your own database server of choice 
+
+# DATABASE_URI: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+
+
+#examples: 
+# DATABASE_URI = postgresql+psycopg2://scott:abc123@localhost/mydatabase
+# DATABASE_URI = mysql+mysqldb://scott:tiger@localhost/foo
