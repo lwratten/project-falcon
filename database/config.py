aae2f346fe994a5a3fc9370ff0e863027e4081eb
Kento-Bento <71308285+Kento-Bento@users.noreply.github.com>
2020-10-18 21:24:35 +1100

    Add connect command
    # connect command allows user to modify the config.py DATABASE_URI with their username, password and database name
    # if the user has not yet made a falcon_multiqc database, this command allows them to make a new one
    
    TODO: The error handling for 'operationerror' does not work and I don't know how to make it capture that error. The error will occur is the user enters invalid username or password during connection. Ideally, the error handling should capture this error and let the user try again.

--- database/config.py
+++ database/config.py
@@ -1,8 +1,2 @@
 ### config.py ###
 
-# For now, to test the database locally, you will have to set the URI below to log into your own database server of choice.
-# TODO Uncomment the below with your own server's information.
-# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-
-# DATABASE_URI = "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-DATABASE_URI = "postgres+psycopg2://postgres:bentopostgres@localhost:5432/megaqc1"
---
+++ falcon_multiqc/commands/connect.py
@@ -0,0 +1,54 @@
+import click
+from getpass import getpass
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import make_url
+from sqlalchemy.exc import OperationalError
+
+# connect command allows user to modify the config.py DATABASE_URI with their username, password and database name
+# if the user has not yet made a falcon_multiqc database, this command allows them to make a new one
+
+# function to modify the config.py file with a new DATABASE_URI for the current user
+def create_config(username, password, server_address, database):
+    with open("database/config.py", 'w') as config_file:
+        config_file.write("### config.py ###\n\n")
+        if server_address:
+            config_file.write(f'DATABASE_URI = "postgres+psycopg2://{username}:{password}@{server_address[0]}:{server_address[1]}/{database}"')
+        else:
+            config_file.write(f'DATABASE_URI = "postgres+psycopg2://{username}:{password}@localhost:5432/{database}"')
+
+@click.command()
+@click.option("-a", "--server_address", type=click.STRING, required=False, help="Enter IPV4 and port for postgres server")
+def cli(server_address):
+    """Connects the user to a postgres database, creates new database one doesn't exist"""
+
+    click.echo("Please enter the username and password for your postgres server\n")
+    while True:
+        username = click.prompt("Enter username (e.g. postgres)")
+        password = getpass("Enter password: ")
+        try:
+            if server_address:
+                server_address = server_address.split()
+                login_engine = create_engine(f'postgres+psycopg2://{username}:{password}@{server_address[0]}:{server_address[1]}')
+            else:
+                login_engine = create_engine(f'postgres+psycopg2://{username}:{password}@localhost:5432')
+            break
+        except OperationalError:
+            click.echo("Username or password could not connect to postgres server\n")
+    with login_engine.connect() as conn:
+        databases = list(conn.execute("SELECT datname FROM pg_database WHERE datistemplate = false;")) # queries postgreSQL for list of avaliable databases
+        click.echo(f'\nAvaliable databases under {username} user are:')
+        [click.echo(f'{d[0]}') for d in databases]
+
+        database = click.prompt("Enter database name to connect, or enter new name to create a new falcon_multiqc database")
+        if (database,) in databases:
+            create_config(username, password, server_address, database) # re-create config file with proper connection URL 
+        else:
+            """Creates a new database"""
+            create_config(username, password, server_address, database) 
+            conn.execute("commit") # databases cannot be created inside transactions, this line will close the previous transaction 
+            conn.execute(f"create database {database}") # create an empty database 
+            from database.crud import create_database
+            create_database() # populate the database with tables 
+            click.echo("Database has been created!")
+        click.echo(f"You are now connected to database {database}!")
+    login_engine.dispose()
