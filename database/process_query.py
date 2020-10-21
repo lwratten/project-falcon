8fcbccaf2d0140b70c991b3a70121a6e068c1f52
Kento-Bento <71308285+Kento-Bento@users.noreply.github.com>
2020-10-21 16:03:44 +1100

    Remade
    From the query command if multiqc flag is used, run the run_multiqc function by giving it the abolute path of the query result.txt + user specified directory to save the multiqc in. If html flag is specified, automatically open the html doc
    
    If csv flag is specified, run the create_csv function. Input is the result of the query which returns all the metrics from a given set of samples
    
    Use print_query function to simply print query

--- database/process_query.py
+++ database/process_query.py
@@ -2,36 +2,23 @@ import os
 import csv
 import subprocess 
 
-#temp: example user or query file paths 
-# user_path = r"D:\C_DRIVE_DATA\falcon_multiqc_DATA\test"
-# query_file = r'C:\Users\New\Desktop\MastersIT\2020T3\BINF6112\Project_Falcon\test2\list_samples.txt'
+# Creates new multiqc report given the absolute path for file_names.txt and the user specified directory
+def run_multiqc(file_names, user_dir, html = None):
+    config = os.path.abspath("./falconqc.config")
+    subprocess.run(['multiqc', '-l', file_names, '-c', config, '-o', user_dir]) 
+    if html:
+        subprocess.call(['xdg-open', user_dir + '/multiqc_report.html']) # for windows: os.startfile(user_dir + '\multiqc_report.html')
 
-def process_query(query_file, *args, **kwargs):
-    # query_file needs to be absolute path
-    # accepted kwargs are: csv, multiqc - can use both at once
-    # For each key, value is the absolute path user wants data to be saved
-    # user can specify 'html' flag during query which will automatically open newly made multiqc HTML file
-    # By deafult this function will stdout 
-
-    if 'csv' in kwargs.keys():
-        with open(query_file, 'r') as metric_list:
-            csv_reader = csv.reader(metric_list)
-            with open(kwargs['csv'] + '\metrics.csv', 'w') as metric_csv:
-                csv_writer = csv.writer(metric_csv, delimiter = ',')
-                for line in csv_reader:
-                    csv_writer.writerow(line)
-
-    if 'multiqc' in kwargs.keys():   
-        config = os.path.abspath("./falconqc.config")
-        subprocess.run(['multiqc', '-l', query_file, '-c', config, '-o', kwargs['multiqc']]) 
-        if 'html' in args:
-            subprocess.call(['wsl-open', kwargs['multiqc'] + '/multiqc_report.html']) # for windows: os.startfile(kwargs['multiqc'] + '\multiqc_report.html')
-    
-    if not kwargs.keys():
-        with open(query_file, 'r') as metric_list:
-            csv_reader = csv.reader(metric_list)
+# Creates new csv files given the metric_list (not sure what this is yet) and the abolute path for the user specified directory 
+def create_csv(metric_list, user_dir):
+    with open(metric_list, 'r') as ml:
+        csv_reader = csv.reader(ml)
+        with open(user_dir + '\metrics.csv', 'w') as metrics_csv:
+            csv_writer = csv.writer(metrics_csv, delimiter = ',')
             for line in csv_reader:
-                print(line)
+                csv_writer.writerow(line)
 
-#temp example: 
-# process_query(query_file, 'html', csv = user_path, multiqc = user_path)
+# Prints the list of samples (as row) returned from query 
+def print_query(sample_list, user_dir):
+    [print(sample_list[i]) for i in sample_list]
+
