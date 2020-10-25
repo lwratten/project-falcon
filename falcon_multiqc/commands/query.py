fdde60628b02c92f578b31c3a208c00811167740
Kento Croft <71308285+Kento-Bento@users.noreply.github.com>
2020-10-25 15:07:09 +1100

    optimisations
    Now all queries will be converted to sets which can be intersected with other query results to allow for user to select and filter on N number of fields

--- falcon_multiqc/commands/query.py
+++ falcon_multiqc/commands/query.py
@@ -14,7 +14,7 @@ from database.process_query import create_new_multiqc
 # verifybamid AVG_DP < 28
 # picard_insertSize MEAN_INSERT_SIZE > 565
 # picard_wgsmetrics PCT_EXC_DUPE <= 0.04
-# next
+# Then hit 'enter'
 
 @click.command()
 @click.option('--select', is_flag=True, required=False, help = "Enter what you want to select on, e.g. select for sample_name, batch, etc. (deafult is sample_name)")
@@ -42,111 +42,118 @@ def cli(select, tool_metric, batch, cohort, multiqc, csv, directory):
             click.echo("If multiqc report is selected, please ensure to select for only sample_name")
             sys.exit(1)
 
-    tm_query = []
+    tm_query_list = []
     if tool_metric:
-        click.echo("Enter any number of queries in the form: <tool metric operator number>\nEnter 'next' to finish\n")
+        click.echo("Enter any number of queries in the form: <tool metric operator number>\nTo exit, enter nothing\n")
         while True:
-            query = click.prompt("Query (tool/metric/operator/number)")
-            if query == 'next':
+            query = input("Query (tool/metric/operator/number): ") # note click.prompt is able to capture the 'enter' key, hence using input()
+            if query == '':
                 break
             if len(query.split()) != 4:
                 click.echo('Incorrect format: Please enter 4 arguments <tool metric operator number> correctly')
                 continue
-            tm_query.append(query)
+            tm_query_list.append(query)
         
 
     # for potential future layout idea
-    b_query = []
+    b_query_list = []
     if batch:
         # code
         pass
 
     # for potential future layout idea
-    c_query = []
+    c_query_list = []
     if cohort:
         #code
         pass
     
 
     click.echo(f"Returning {select} by filtering for:")
-    [click.echo(f'{tm}') for tm in tm_query]
-    [click.echo(f'{b}') for b in b_query]
-    [click.echo(f'{c}') for c in c_query]
+    [click.echo(f'{tm}') for tm in tm_query_list]
+    [click.echo(f'{b}') for b in b_query_list]
+    [click.echo(f'{c}') for c in c_query_list]
 
     # start session 
-    with session_scope() as session:
-        first_loop = True
-        for query in tm_query:
-            query = query.split()
-            tool = query[0]
-            attribute = query[1]
-            operator = query[2]
-            value = query[3] 
-            if operator == '<':
-                if first_loop:
-                    sample_list = session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) < value)
-                else:
-                    sample_list = sample_list.union(session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) < value))
-                session.expire_on_commit = False
-            if operator == '>':
-                if first_loop:
-                    sample_list = session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) > value)
-                else:
-                    sample_list = sample_list.union(session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) > value))
-                session.expire_on_commit = False
-            if operator == '==':
-                if first_loop:
-                    sample_list = session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) == value)
-                else:
-                    sample_list = sample_list.union(session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) == value))
-                session.expire_on_commit = False
-            if operator == '>=':
-                if first_loop:
-                    sample_list = session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) >= value)
-                else:
-                    sample_list = sample_list.union(session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) >= value))
-                session.expire_on_commit = False
-            if operator == '<=':
-                if first_loop:
-                    sample_list = session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) <= value)
-                else:
-                    sample_list = sample_list.union(session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) <= value))
-                session.expire_on_commit = False
-            if operator == '!=':
-                if first_loop:
-                    sample_list = session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) != value)
-                else:
-                    sample_list = sample_list.union(session.query(RawData).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) != value))
-                session.expire_on_commit = False
-            first_loop = False
-        sample_list_tm = sample_list.order_by(RawData.sample_id).all() # creates list containing every row of Rawdata accross database which satifies filtering, ordered by sample_ID
+    if tm_query_list:
+        with session_scope() as session:
+            first_loop = True
+            for query in tm_query_list:
+                query = query.split()
+                tool = query[0]
+                attribute = query[1]
+                operator = query[2]
+                value = query[3] 
+                if operator == '<':
+                    if first_loop:
+                        tm_query = session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) < value)
+                    else:
+                        tm_query = tm_query.union(session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) < value))
+                    session.expire_on_commit = False
+                if operator == '>':
+                    if first_loop:
+                        tm_query = session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) > value)
+                    else:
+                        tm_query = tm_query.union(session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) > value))
+                    session.expire_on_commit = False
+                if operator == '==':
+                    if first_loop:
+                        tm_query = session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) == value)
+                    else:
+                        tm_query = tm_query.union(session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) == value))
+                    session.expire_on_commit = False
+                if operator == '>=':
+                    if first_loop:
+                        tm_query = session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) >= value)
+                    else:
+                        tm_query = tm_query.union(session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) >= value))
+                    session.expire_on_commit = False
+                if operator == '<=':
+                    if first_loop:
+                        tm_query = session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) <= value)
+                    else:
+                        tm_query = tm_query.union(session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) <= value))
+                    session.expire_on_commit = False
+                if operator == '!=':
+                    if first_loop:
+                        tm_query = session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) != value)
+                    else:
+                        tm_query = tm_query.union(session.query(RawData.sample_id).filter(RawData.qc_tool == tool, RawData.metrics[attribute].astext.cast(Float) != value))
+                    session.expire_on_commit = False
+                first_loop = False
+            sample_query_set = set(tm_query.all()) # creates set containing every RawData.sample_id filtered accross database which satifies filtering, this acts as a global query set for samples
 
-        # for potential future layout idea
-        first_loop = True
-        sample_list = None
-        for query in b_query:
-            pass
-        sample_list_b = []
+    # for potential future layout idea
+    if b_query_list:
+        with session_scope() as session: 
+            first_loop = True
+            b_query = None
+            for query in b_query:
+                pass
+            if sample_query_set: # if the global sample query set is NOT empty, then get intersection with the batch sample query result 
+                sample_query_set = sample_query_set.intersection(sample_query_set, set(b_query.all()))
+            else:
+                sample_query_set = set(b_query.all()) # if the global sample query set IS empty, then populate it with the batch query result etc. 
 
-        # for potential future layout idea
-        first_loop = True
-        sample_list = None
-        for query in c_query:
-            pass
-        sample_list_c = []
+    # for potential future layout idea
+    if c_query_list:
+        with session_scope() as session:
+            first_loop = True
+            c_query = None
+            for query in c_query:
+                pass
+            if sample_query_set:
+                sample_query_set = sample_query_set.intersection(sample_query_set, set(c_query.all()))
+            else:
+                sample_query_set = set(c_query.all())
 
     # if select contains sample_name, list of sample_name/path will be made from the query
     if 'sample_name' in select:
-        prev = None
-        sample_path_list = []
-        click.echo(f'total query was: {len(sample_list_tm)}') # temp
-        for row in sample_list_tm:
-            if row.sample_id == prev:
-                continue # filters out duplicates - NOTE duplicates means sample_name satifies 2 or more of the filters which could be an additional filtering option to add
-            prev = row.sample_id
-            sample_name, path = session.query(Sample.sample_name, Batch.path).join(Sample).filter(Sample.id == row.sample_id).options(Load(Batch).load_only('path')).first()
-            sample_path_list.append((sample_name, path)) # list of tuples
-
+        with session_scope() as session:
+            sample_path_list = []
+            click.echo(f'total query was: {len(sample_query_set)}') # temp
+            for sample_id in sample_query_set: # this set will contain only sample_IDs which satisfy all fliter conditions (i.e. tool/metric/batch/cohort)
+                sample_name, path = session.query(Sample.sample_name, Batch.path).join(Batch).filter(Sample.id == sample_id[0]).first()
+                sample_path_list.append((sample_name, path)) # list of tuples
 
     if multiqc:
         click.echo("creating multiqc report...")
