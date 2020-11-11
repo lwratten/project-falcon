22cec5719c0f674690b8f4fd91c2f6a4ecd144a9
Kento Croft <71308285+Kento-Bento@users.noreply.github.com>
2020-11-12 02:03:47 +1100

    Update remove.py

--- falcon_multiqc/commands/remove.py
+++ falcon_multiqc/commands/remove.py
@@ -22,24 +22,24 @@ def cli(cohort, batch, overview):
             for cohort_id in cohort:
                 if session.query(Cohort.id).filter(Cohort.id == cohort_id).scalar() is None:  
                     # Roll back of all changes made during this session.          
-                    raise Exception(f"No cohort {cohort_id} is present in the database."
+                    raise Exception(f"No cohort {cohort_id} is present in the database. Nothing has been deleted."
                                     "\nRun --overview option to see what is currently present.")
                 else:
                     # Delete all assoicated rows
                     session.query(Cohort.id).filter(Cohort.id == cohort_id).delete()
-                    click.echo(f"Cohort {cohort_id} and all assoicated entries have been deleted.")
                     if batch:
                         click.echo("\nWarning: if both --cohort and --batch are used together, only --cohort will execute."
-                        f"\nThe following will not be deleted: {list(batch)}")
+                        f"\nThe following will not be executed, but may have been deleted: {list(batch)}")
+            click.echo(f"Cohort(s) {list(cohort)} and all assoicated entries have been deleted.")
         elif batch:
             for cohort_id, batch_name in batch:
                 if session.query(Batch.id).filter(Batch.batch_name == batch_name,Batch.cohort_id == cohort_id).scalar() is None:
                     # Roll back of all changes made during this session.          
-                    raise Exception(f"No batch {batch_name} is present in the database."
+                    raise Exception(f"No batch {batch_name} is present in the database. Nothing has been deleted."
                                     "\nRun --overview option to see what is currently present.")
                 else:
                     # Delete all assoicated rows
                     session.query(Batch.id).filter(Batch.batch_name == batch_name,Batch.cohort_id == cohort_id).delete()
-                    click.echo(f"Batch {batch_name} and all assoicated entries have been deleted.")
+            click.echo(f"Batch(s) {list(batch)} and all assoicated entries have been deleted.")
     if overview:
         print_overview(session)
