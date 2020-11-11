0b75a318f2986fd42c5a08186c557419c1fac64e
Kento Croft <71308285+Kento-Bento@users.noreply.github.com>
2020-11-12 01:40:14 +1100

    Remove command
    In order for remove command to work, first ensure your database is remade by using the recreate_tables command, because some changes have been made to the models.py (added cascade ondelete)
    
    When using Remove, recommended to first check out the database by: falcon_multiqc remove --overview
    
    To remove a particular cohort (or multiple) and all assoicated rows do:
    falcon_multiqc remove -c MGRB
    
    To remove some batches, do:
    falcon_multiqc remove -b Cohort2  19_Aug20_CAGRF20021238-2 -b Cohort2 9_Oct20_CAGRF20021238-7

---
+++ falcon_multiqc/commands/remove.py
@@ -0,0 +1,45 @@
+import click
+from database.crud import session_scope
+from sqlalchemy.orm import Query
+from .query import print_overview
+from database.models import Base, Batch, Cohort
+
+# Command for removing entries from database.
+# --overview to see all the cohorts with there respective batches and number of samples
+# --cohort option requires one argument 'cohortID' - removes all entries associated with that cohort throughout db
+# --batch option requires two arguments 'cohortID and Batch' - removes all entries associated with that batch throughout db
+# Note: although you can use multiple --batch or --cohort options in one command, if both are used together, only --cohort will commit
+
+@click.option("-c", "--cohort", multiple=True, required=False, help="Which cohort to remove from the database. E.g. <MGRB>")
+@click.option ("-b", "--batch", multiple=True, type=(str, str), required=False, help="Which batch you want to remove from the database. E.g. <MGRB BAB>")
+@click.option("-ov", "--overview", is_flag=True, required=False, help="Prints an overview of the number of samples in each batch/cohort.")
+@click.command()
+def cli(cohort, batch, overview):
+    """Removes all associated rows of specified batch/cohort from database."""
+
+    with session_scope() as session:
+        if cohort:
+            for cohort_id in cohort:
+                if session.query(Cohort.id).filter(Cohort.id == cohort_id).scalar() is None:  
+                    # Roll back of all changes made during this session.          
+                    raise Exception(f"No cohort {cohort_id} is present in the database."
+                                    "\nRun --overview option to see what is currently present.")
+                else:
+                    # Delete all assoicated rows
+                    session.query(Cohort.id).filter(Cohort.id == cohort_id).delete()
+                    click.echo(f"Cohort {cohort_id} and all assoicated entries have been deleted.")
+                    if batch:
+                        click.echo("\nWarning: if both --cohort and --batch are used together, only --cohort will execute."
+                        f"\nThe following will not be deleted: {list(batch)}")
+        elif batch:
+            for cohort_id, batch_name in batch:
+                if session.query(Batch.id).filter(Batch.batch_name == batch_name,Batch.cohort_id == cohort_id).scalar() is None:
+                    # Roll back of all changes made during this session.          
+                    raise Exception(f"No batch {batch_name} is present in the database."
+                                    "\nRun --overview option to see what is currently present.")
+                else:
+                    # Delete all assoicated rows
+                    session.query(Batch.id).filter(Batch.batch_name == batch_name,Batch.cohort_id == cohort_id).delete()
+                    click.echo(f"Batch {batch_name} and all assoicated entries have been deleted.")
+    if overview:
+        print_overview(session)
