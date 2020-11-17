70ae846a370cc88a557b5a34e8f15ebef8ce7e13
Kento Croft <71308285+Kento-Bento@users.noreply.github.com>
2020-11-17 21:00:39 +1100

    Update save.py

--- falcon_multiqc/commands/save.py
+++ falcon_multiqc/commands/save.py
@@ -199,7 +199,7 @@ stripChars = " \n\r\t\'\""
 def cli(directory, sample_metadata, input_csv, batch_description, cohort_description, batch_metadata, cohort_metadata):
     """Saves the given cohort directory to the falcon_multiqc database"""
 
-    if (not directory and not input_csv) and (not batch_metadata or cohort_metadata):
+    if (not directory and not input_csv) and not (batch_metadata or cohort_metadata):
         raise Exception("Save requires either an input --directory OR --input_csv.")
     if directory and input_csv:
        raise Exception("Save requires only one of input --directory OR --input_csv, not both.")
